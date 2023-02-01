c2c9d5d49f3e74566323ee0cd0e8e538ee81262c
flarnrules <breifenberg11@gmail.com>
2023-01-31 21:34:05 -0600

    Start of 'Programming a Guessing Game', chapter 2 of Rust Book

---
+++ projects/guessing_game/src/main.rs
@@ -0,0 +1,16 @@
+use std:io
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); // String is a string type that is growable
+
+    io::stdin()
+        .read_line(&mut guess) // Puts whatever the user enters into the string we pass to it, but also returns a `Result` value
+        // Result is an enumeration (enum for short), which is a type that can be in one of multiple possible states. Each possible state is called a variant.
+        .expect("Failed to read line"); // It's good to introduce a new line when you call a method with .method_name() syntax
+    
+    println!("You guessed: {guess}");
+}
---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/flarnrules/repos/rust_book/projects/guessing_game/target/debug/guessing_game: /home/flarnrules/repos/rust_book/projects/guessing_game/src/main.rs
