5f3ed227dbfaf4ca790af8011f6dd9ad9fccb908
flarnrules <breifenberg11@gmail.com>
2023-02-02 19:15:34 -0600

    Revise main.rs

---
+++ projects/2_guessing_game/target/debug/deps/rand_chacha-cad8015603ae1161.d
@@ -0,0 +1,7 @@
+/home/flarnrules/repos/rust_book/projects/2_guessing_game/target/debug/deps/rand_chacha-cad8015603ae1161.rmeta: /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/home/flarnrules/repos/rust_book/projects/2_guessing_game/target/debug/deps/rand_chacha-cad8015603ae1161.d: /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
+/home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
+/home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
