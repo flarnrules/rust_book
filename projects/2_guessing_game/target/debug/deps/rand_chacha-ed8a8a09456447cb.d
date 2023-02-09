8dfc851bc2e5fbb8ccabda85a61a0330071bcdaf
flarnrules <breifenberg11@gmail.com>
2023-02-09 16:19:38 -0600

    rename some files

--- projects/2_guessing_game/target/debug/deps/rand_chacha-ed8a8a09456447cb.d
+++
@@ -1,9 +0,0 @@
-/home/flarnrules/repos/rust_book/projects/2_guessing_game/target/debug/deps/rand_chacha-ed8a8a09456447cb.rmeta: /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/flarnrules/repos/rust_book/projects/2_guessing_game/target/debug/deps/librand_chacha-ed8a8a09456447cb.rlib: /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/flarnrules/repos/rust_book/projects/2_guessing_game/target/debug/deps/rand_chacha-ed8a8a09456447cb.d: /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
-/home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
-/home/flarnrules/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
--- projects/2_guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/flarnrules/repos/rust_book/projects/2_guessing_game/target/debug/guessing_game: /home/flarnrules/repos/rust_book/projects/2_guessing_game/src/main.rs
