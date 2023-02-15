9989dd728abf891cdb579eacff8d50a336ae7bf4
flarnrules <breifenberg11@gmail.com>
2023-02-15 12:32:59 -0600

    Delete all projects to start over

--- projects/2_guessing_game/target/debug/deps/guessing_game-f243038d37031439.d
+++
@@ -1,5 +0,0 @@
-/home/flarnrules/repos/rust_book/projects/2_guessing_game/target/debug/deps/guessing_game-f243038d37031439.rmeta: src/main.rs
-
-/home/flarnrules/repos/rust_book/projects/2_guessing_game/target/debug/deps/guessing_game-f243038d37031439.d: src/main.rs
-
-src/main.rs:
