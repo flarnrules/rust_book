c45495eb9aec91856b6879a30de4dbda2d75e42b
flarnrules <breifenberg11@gmail.com>
2023-02-21 17:30:27 -0600

    Write another tuple

--- tuples/src/main.rs
+++ tuples/src/main.rs
@@ -8,4 +8,16 @@ fn main() {
     println!("The value of x is; {x}");
 
     println!("The value of z is; {z}");
+
+    let point: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = point.0;
+
+    let six_point_four = point.1;
+
+    let one = point.2;
+
+    print!("The values in my new tuple are");
+
 }
+
