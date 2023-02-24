745de05d170bbf0a95df10e5a89dd246a9407de1
flarnrules <breifenberg11@gmail.com>
2023-02-23 22:07:58 -0600

    Remove target directory from git repo

--- arrays/target/debug/arrays.d
+++
@@ -1,1 +0,0 @@
-/home/flarnrules/repos/rust_book/arrays/target/debug/arrays: /home/flarnrules/repos/rust_book/arrays/src/main.rs
--- arrays/target/debug/deps/arrays-ebdc784c3fb95ead.d
+++
@@ -1,5 +0,0 @@
-/home/flarnrules/repos/rust_book/arrays/target/debug/deps/arrays-ebdc784c3fb95ead: src/main.rs
-
-/home/flarnrules/repos/rust_book/arrays/target/debug/deps/arrays-ebdc784c3fb95ead.d: src/main.rs
-
-src/main.rs:
