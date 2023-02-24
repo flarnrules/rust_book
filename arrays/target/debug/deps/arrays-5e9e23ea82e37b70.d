00952763c405e1b6e2a857db20f7160e913c8c42
flarnrules <breifenberg11@gmail.com>
2023-02-23 22:11:38 -0600

    Get rid of target from git repo

--- arrays/target/debug/deps/arrays-5e9e23ea82e37b70.d
+++
@@ -1,5 +0,0 @@
-/home/flarnrules/repos/rust_book/arrays/target/debug/deps/arrays-5e9e23ea82e37b70.rmeta: src/main.rs
-
-/home/flarnrules/repos/rust_book/arrays/target/debug/deps/arrays-5e9e23ea82e37b70.d: src/main.rs
-
-src/main.rs:
